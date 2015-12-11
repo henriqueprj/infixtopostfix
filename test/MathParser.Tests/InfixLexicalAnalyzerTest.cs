9ba8441a3bebe5133c5f5639cb2d6178dc4a7083
Henrique Ribeiro <henriqueprj@gmail.com>
2015-12-11 19:31:19 -0300

    Include Fact attribute on test method

--- test/MathParser.Tests/InfixLexicalAnalyzerTest.cs
+++ test/MathParser.Tests/InfixLexicalAnalyzerTest.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Xunit;
 
 namespace MathParser.Tests
 {
     public class InfixLexicalAnalyzerTest
     {
+        [Fact]
         public void Expressao_binaria_simples_com_espacos()
         {
             //var analyzer = new InfixLexicalAnalyzer();
